f4d358a46809e8e33eeace8114e6349916553c08
Tom Bloor <tom.bloor@googlemail.com>
2016-12-25 16:47:24 +0000

    Solved Day06 Puzzle 01

---
+++ lib/Solver/Day06/Puzzle01.pm
@@ -0,0 +1,30 @@
+package Solver::Day06::Puzzle01;
+
+use Moo;
+use Devel::Dwarn;
+
+sub run {
+  my ( $self, $input ) = @_;
+
+  my @lines = split "\n", $input;
+
+  my @hashes; push( @hashes, {} ) for 1 .. length( $lines[0] );
+
+  for my $line ( @lines ) {
+    for my $index ( 0 .. length( $line ) - 1) {
+      my $char = substr $line, $index, 1;
+      $hashes[$index]->{$char}++;
+    }
+  }
+
+  my $message = '';
+
+  for my $hash ( @hashes ) {
+    my ( $char ) = sort { $hash->{$b} <=> $hash->{$a} } keys %$hash;
+    $message .= $char;
+  }
+
+  return $message;
+}
+
+1;
